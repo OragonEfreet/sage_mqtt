fda74641c92cc2a7935df8d72c1ba63c56db3968
Kevin D'ORANGE <kevin.dorange@gmail.com>
2020-03-25 13:28:14 +0100

    MQTT-1.5.4-1 compliance

--- src/encode.rs
+++ src/encode.rs
@@ -1,7 +1,7 @@
 //! Defines the `Encode` trait and implement it for the MQTT types.
 
 use crate::{
-    BinaryData, Bits, FourByteInteger, Result as MyResult, TwoByteInteger, UTF8String,
+    BinaryData, Bits, Error, FourByteInteger, Result as MyResult, TwoByteInteger, UTF8String,
     VariableByteInteger,
 };
 use std::io::{Error as IOError, ErrorKind, Write};
@@ -40,13 +40,18 @@ impl Encode for UTF8String {
         W: Write,
     {
         let data = &self.0;
-        let len = data.len();
-        if len > i16::max_value() as usize {
-            return Err(IOError::new(ErrorKind::InvalidData, "Something went wrong").into());
+
+        if let Ok(_) = String::from_utf8(data.to_vec()) {
+            let len = data.len();
+            if len > i16::max_value() as usize {
+                return Err(Error::MalformedPacket);
+            }
+            writer.write_all(&(len as u16).to_be_bytes())?;
+            writer.write_all(data)?;
+            Ok(2 + len)
+        } else {
+            Err(Error::MalformedPacket)
         }
-        writer.write_all(&(len as u16).to_be_bytes())?;
-        writer.write_all(data)?;
-        Ok(2 + len)
     }
 }
 
@@ -89,7 +94,7 @@ mod unit_encode {
     #[test]
     fn encode_bits() {
         let input = Bits(0b00101010);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x2A];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 1);
@@ -99,7 +104,7 @@ mod unit_encode {
     #[test]
     fn encode_two_byte_integer() {
         let input = TwoByteInteger(1984u16);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x07, 0xC0];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 2);
@@ -109,7 +114,7 @@ mod unit_encode {
     #[test]
     fn encode_four_byte_integer() {
         let input = FourByteInteger(220_000_u32);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x00, 0x03, 0x5B, 0x60];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 4);
@@ -119,7 +124,7 @@ mod unit_encode {
     #[test]
     fn encode_utf8_string() {
         let input = UTF8String(Vec::from("A𪛔".as_bytes()));
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x00, 0x05, 0x41, 0xF0, 0xAA, 0x9B, 0x94];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 7);
@@ -129,17 +134,31 @@ mod unit_encode {
     #[test]
     fn encode_utf8_string_empty() {
         let input = UTF8String(Vec::new());
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x00, 0x00];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 2);
         assert_eq!(result, expected, "Encoding {:?} failed", input);
     }
 
+    // The character data in a UTF-8 Encoded String MUST be well-formed UTF-8 as
+    // defined by the Unicode specification [Unicode] and restated in
+    // RFC 3629 [RFC3629]. In particular, the character data MUST NOT include
+    // encodings of code points between U+D800 and U+DFFF [MQTT-1.5.4-1]
+    #[test]
+    fn conformance_mqtt_1_5_4_1() {
+        let input = UTF8String(vec![0xD8, 0x00]);
+        let mut test_stream = Vec::new();
+        assert!(matches!(
+            input.encode(&mut test_stream),
+            Err(Error::MalformedPacket)
+        ));
+    }
+
     #[test]
     fn encode_variable_byte_integer_one_lower_bound() {
         let input = VariableByteInteger::One(0_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x00];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 1);
@@ -149,7 +168,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_one_upper_bound() {
         let input = VariableByteInteger::One(127_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x7F];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 1);
@@ -159,7 +178,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_two_lower_bound() {
         let input = VariableByteInteger::Two(128_u8, 01_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x80, 0x01];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 2);
@@ -169,7 +188,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_two_upper_bound() {
         let input = VariableByteInteger::Two(255_u8, 127_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0xFF, 0x7F];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 2);
@@ -179,7 +198,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_three_lower_bound() {
         let input = VariableByteInteger::Three(128_u8, 128_u8, 01_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x80, 0x80, 0x01];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 3);
@@ -189,7 +208,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_three_upper_bound() {
         let input = VariableByteInteger::Three(255_u8, 255_u8, 127_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0xFF, 0xFF, 0x7F];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 3);
@@ -199,7 +218,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_four_lower_bound() {
         let input = VariableByteInteger::Four(128_u8, 128_u8, 128_u8, 01_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x80, 0x80, 0x80, 0x01];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 4);
@@ -209,7 +228,7 @@ mod unit_encode {
     #[test]
     fn encode_variable_byte_integer_four_upper_bound() {
         let input = VariableByteInteger::Four(255_u8, 255_u8, 255_u8, 127_u8);
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0xFF, 0xFF, 0xFF, 0x7F];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 4);
@@ -219,7 +238,7 @@ mod unit_encode {
     #[test]
     fn encode_binarydata() {
         let input = BinaryData(Vec::from("A𪛔".as_bytes()));
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x00, 0x05, 0x41, 0xF0, 0xAA, 0x9B, 0x94];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 7);
@@ -229,7 +248,7 @@ mod unit_encode {
     #[test]
     fn encode_binarydata_empty() {
         let input = BinaryData(Vec::new());
-        let mut result: Vec<u8> = Vec::new();
+        let mut result = Vec::new();
         let expected = vec![0x00, 0x00];
         let bytes = input.encode(&mut result).unwrap();
         assert_eq!(bytes, 2);
