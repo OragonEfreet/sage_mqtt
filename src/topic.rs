5e41f161dcea1481a88174b06c8aef7add9a192a
Kevin d'Orange <kdorange@techsoft3d.com>
2021-12-22 10:49:32 +0100

    Bunch of tests for topic name

--- src/topic.rs
+++ src/topic.rs
@@ -3,9 +3,17 @@ use std::fmt;
 const LEVEL_SEPARATOR: char = '/';
 
 /// A topic name a broker or client publishes to
-#[derive(Default, Hash, Debug, Eq, PartialEq, Clone)]
+#[derive(Hash, Debug, Eq, PartialEq, Clone)]
 pub struct TopicName {
     spec: Vec<TopicLevel>,
+}
+
+impl Default for TopicName {
+    fn default() -> Self {
+        TopicName {
+            spec: vec![TopicLevel::Empty],
+        }
+    }
 }
 
 impl From<String> for TopicName {
@@ -129,5 +137,65 @@ impl TopicFilter {
     /// Returns the sharing name if the topic is shared
     pub fn share(&self) -> &Option<String> {
         &self.share
+    }
+}
+
+#[cfg(test)]
+mod unit {
+    use super::*;
+
+    macro_rules! topic_name_data {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                mod $name {
+                    use super::*;
+                    #[test]
+                    fn from_string() {
+                        let (input, spec) = $value;
+                        assert_eq!(TopicName::from(String::from(input)), TopicName {spec});
+                    }
+
+                    #[test]
+                    fn from_str_ref() {
+                        let (input, spec) = $value;
+                        assert_eq!(TopicName::from(input), TopicName {spec});
+                    }
+
+                    #[test]
+                    fn to_string() {
+                        let (input, spec) = $value;
+                        assert_eq!(TopicName {spec}.to_string(), input);
+                    }
+                }
+            )*
+        }
+    }
+
+    topic_name_data! {
+        default:          (String::default(), vec![TopicLevel::Empty], ),
+        space:            (" ",               vec![TopicLevel::Name(String::from(" "))], ),
+        empty_1:          ("",                vec![TopicLevel::Empty ; 1], ),
+        empty_2:          ("/",               vec![TopicLevel::Empty ; 2], ),
+        empty_3:          ("//",              vec![TopicLevel::Empty ; 3], ),
+        single:           ("jaden",           vec![TopicLevel::Name(String::from("jaden"))], ),
+        single_head:      ("/jaden",          vec![TopicLevel::Empty, TopicLevel::Name(String::from("jaden"))], ),
+        single_tail:      ("jaden/",          vec![TopicLevel::Name(String::from("jaden")), TopicLevel::Empty], ),
+        single_wrapped:   ("/jaden/",         vec![TopicLevel::Empty, TopicLevel::Name(String::from("jaden")), TopicLevel::Empty], ),
+        multiple:         ("jaden/jarod",     vec![TopicLevel::Name(String::from("jaden")), TopicLevel::Name(String::from("jarod"))], ),
+        multiple_head:    ("/jaden/jarod",    vec![TopicLevel::Empty, TopicLevel::Name(String::from("jaden")), TopicLevel::Name(String::from("jarod"))], ),
+        multiple_tail:    ("jaden/jarod/",    vec![TopicLevel::Name(String::from("jaden")), TopicLevel::Name(String::from("jarod")), TopicLevel::Empty], ),
+        multiple_wrapped: ("/jaden/jarod/",   vec![TopicLevel::Empty, TopicLevel::Name(String::from("jaden")), TopicLevel::Name(String::from("jarod")), TopicLevel::Empty], ),
+        wildcard_plus:    ("+",               vec![TopicLevel::Name(String::from("+"))], ),
+        wildcard_pound:   ("#",               vec![TopicLevel::Name(String::from("#"))], ),
+    }
+
+    #[test]
+    fn default_is_empty() {
+        assert_eq!(
+            TopicName::default(),
+            TopicName {
+                spec: vec![TopicLevel::Empty],
+            },
+        );
     }
 }
