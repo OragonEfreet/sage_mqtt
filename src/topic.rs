9b2642cb7d9e3a43b629cd723d380df520aeae1a
Kevin d'Orange <kdorange@techsoft3d.com>
2021-12-22 15:05:37 +0100

    Topic supports parsing of share

--- src/topic.rs
+++ src/topic.rs
@@ -6,6 +6,7 @@ const LEVEL_SEPARATOR: char = '/';
 enum TopicLevel {
     Empty,
     Name(String),
+    Share(String),
     Any,
     MultipleAny,
 }
@@ -34,6 +35,7 @@ impl fmt::Display for Topic {
                 .map(|l| match l {
                     TopicLevel::Empty => "",
                     TopicLevel::Name(s) => s.as_ref(),
+                    TopicLevel::Share(_) => "Shareuh",
                     TopicLevel::Any => "+",
                     TopicLevel::MultipleAny => "#",
                 })
@@ -52,23 +54,49 @@ impl From<String> for Topic {
 impl From<&str> for Topic {
     /// Builds a new topic has a name
     fn from(s: &str) -> Self {
-        Topic {
-            spec: s
-                .split(LEVEL_SEPARATOR)
-                .into_iter()
-                .map(|l| {
+        let (mut shared, topic) = {
+            let stripped = s.strip_prefix("$share/");
+            (stripped.is_some(), stripped.unwrap_or(s))
+        };
+
+        let spec: Vec<TopicLevel> = topic
+            .split(LEVEL_SEPARATOR)
+            .into_iter()
+            .map(|l| {
+                if shared {
+                    shared = false;
+                    Some(TopicLevel::Share(l.into()))
+                } else {
                     if l.len() == 0 {
-                        TopicLevel::Empty
+                        Some(TopicLevel::Empty)
                     } else {
                         match l {
-                            "+" => TopicLevel::Any,
-                            "#" => TopicLevel::MultipleAny,
-                            _ => TopicLevel::Name(l.into()),
+                            "+" => Some(TopicLevel::Any),
+                            "#" => Some(TopicLevel::MultipleAny),
+                            _ => Some(TopicLevel::Name(l.into())),
                         }
                     }
-                })
-                .collect(),
-        }
+                }
+            })
+            .filter_map(|e| e)
+            .collect();
+        // TODO maybe use fold instead
+
+        Topic { spec }
+    }
+}
+
+impl Topic {
+    /// Returns the name of the share if any
+    pub fn share(&self) -> Option<String> {
+        None
+    }
+
+    /// Checks whether the topic contains any wildcard
+    pub fn has_wildcards(&self) -> bool {
+        self.spec
+            .iter()
+            .any(|l| matches!(l, TopicLevel::Any | TopicLevel::MultipleAny))
     }
 }
 
@@ -76,7 +104,7 @@ impl From<&str> for Topic {
 mod unit {
     use super::*;
 
-    macro_rules! topic_name_data {
+    macro_rules! test_data {
         ($($name:ident: $value:expr,)*) => {
             $(
                 mod $name {
@@ -100,7 +128,7 @@ mod unit {
 
     use super::TopicLevel::*;
 
-    topic_name_data! {
+    test_data! {
         space:            (" ",               vec![Name(" ".into())], ),
         empty_1:          ("",                vec![Empty ; 1], ),
         empty_2:          ("/",               vec![Empty ; 2], ),
@@ -115,6 +143,23 @@ mod unit {
         multiple_wrapped: ("/jaden/jarod/",   vec![Empty, Name("jaden".into()), Name("jarod".into()), Empty], ),
         wildcard_plus:    ("+",               vec![Any], ),
         wildcard_pound:   ("#",               vec![MultipleAny], ),
+        // Shared
+        share_space:            ("$share/ ",               vec![Share(" ".into())], ),
+        share_empty_1:          ("$share/",                vec![Share("".into())], ),
+        share_empty_2:          ("$share//",               vec![Share("".into()), Empty], ),
+        share_empty_3:          ("$share///",              vec![Share("".into()), Empty, Empty], ),
+        share_single:           ("$share/jaden",           vec![Share("jaden".into())], ),
+        share_single_head:      ("$share//jaden",          vec![Share("".into()), Name("jaden".into())], ),
+        share_single_tail:      ("$share/jaden/",          vec![Share("jaden".into()), Empty], ),
+        share_single_wrapped:   ("$share//jaden/",         vec![Share("".into()), Name("jaden".into()), Empty], ),
+        share_multiple:         ("$share/jaden/jarod",     vec![Share("jaden".into()), Name("jarod".into())], ),
+        share_multiple_head:    ("$share//jaden/jarod",    vec![Share("".into()), Name("jaden".into()), Name("jarod".into())], ),
+        share_multiple_tail:    ("$share/jaden/jarod/",    vec![Share("jaden".into()), Name("jarod".into()), Empty], ),
+        share_multiple_wrapped: ("$share//jaden/jarod/",   vec![Share("".into()), Name("jaden".into()), Name("jarod".into()), Empty], ),
+        share_wildcard_plus_1:  ("$share/+",               vec![Share("+".into())], ),
+        share_wildcard_pound_1: ("$share/#",               vec![Share("#".into())], ),
+        share_wildcard_plus_2:  ("$share/+/+",             vec![Share("+".into()), Any], ),
+        share_wildcard_pound_2: ("$share/#/#",             vec![Share("#".into()), MultipleAny], ),
     }
 
     #[test]
