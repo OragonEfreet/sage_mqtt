3fa70a4f82e78a3e94130d6657d662b7aa5ebc5b
Kevin d'Orange <kdorange@techsoft3d.com>
2021-12-22 11:19:25 +0100

    Add tests for TopicFilter

--- src/topic/filter.rs
+++ src/topic/filter.rs
@@ -76,3 +76,63 @@ impl TopicFilter {
         &self.share
     }
 }
+
+#[cfg(test)]
+mod unit {
+    use super::*;
+
+    macro_rules! topic_filter_data {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                mod $name {
+                    use super::*;
+                    #[test]
+                    fn from_string() {
+                        let (input, spec) = $value;
+                        assert_eq!(TopicFilter::from(String::from(input)), TopicFilter {share: None, spec});
+                    }
+
+                    #[test]
+                    fn from_str_ref() {
+                        let (input, spec) = $value;
+                        assert_eq!(TopicFilter::from(input), TopicFilter {share: None, spec});
+                    }
+
+                    #[test]
+                    fn to_string() {
+                        let (input, spec) = $value;
+                        assert_eq!(TopicFilter {share: None, spec}.to_string(), input);
+                    }
+                }
+            )*
+        }
+    }
+
+    topic_filter_data! {
+        default:          (String::default(), vec![FilterSegment::Level(TopicLevel::Empty)], ),
+        space:            (" ",               vec![FilterSegment::Level(TopicLevel::Name(String::from(" ")))], ),
+        empty_1:          ("",                vec![FilterSegment::Level(TopicLevel::Empty) ; 1], ),
+        empty_2:          ("/",               vec![FilterSegment::Level(TopicLevel::Empty) ; 2], ),
+        empty_3:          ("//",              vec![FilterSegment::Level(TopicLevel::Empty) ; 3], ),
+        single:           ("jaden",           vec![FilterSegment::Level(TopicLevel::Name(String::from("jaden")))], ),
+        single_head:      ("/jaden",          vec![FilterSegment::Level(TopicLevel::Empty), FilterSegment::Level(TopicLevel::Name(String::from("jaden")))], ),
+        single_tail:      ("jaden/",          vec![FilterSegment::Level(TopicLevel::Name(String::from("jaden"))), FilterSegment::Level(TopicLevel::Empty)], ),
+        single_wrapped:   ("/jaden/",         vec![FilterSegment::Level(TopicLevel::Empty), FilterSegment::Level(TopicLevel::Name(String::from("jaden"))), FilterSegment::Level(TopicLevel::Empty)], ),
+        multiple:         ("jaden/jarod",     vec![FilterSegment::Level(TopicLevel::Name(String::from("jaden"))), FilterSegment::Level(TopicLevel::Name(String::from("jarod")))], ),
+        multiple_head:    ("/jaden/jarod",    vec![FilterSegment::Level(TopicLevel::Empty), FilterSegment::Level(TopicLevel::Name(String::from("jaden"))), FilterSegment::Level(TopicLevel::Name(String::from("jarod")))], ),
+        multiple_tail:    ("jaden/jarod/",    vec![FilterSegment::Level(TopicLevel::Name(String::from("jaden"))), FilterSegment::Level(TopicLevel::Name(String::from("jarod"))), FilterSegment::Level(TopicLevel::Empty)], ),
+        multiple_wrapped: ("/jaden/jarod/",   vec![FilterSegment::Level(TopicLevel::Empty), FilterSegment::Level(TopicLevel::Name(String::from("jaden"))), FilterSegment::Level(TopicLevel::Name(String::from("jarod"))), FilterSegment::Level(TopicLevel::Empty)], ),
+        wildcard_plus:    ("+",               vec![FilterSegment::Any], ),
+        wildcard_pound:   ("#",               vec![FilterSegment::MultipleAny],),
+    }
+
+    #[test]
+    fn default_is_empty() {
+        assert_eq!(
+            TopicName::default(),
+            TopicName {
+                spec: vec![TopicLevel::Empty],
+            },
+        );
+    }
+}
